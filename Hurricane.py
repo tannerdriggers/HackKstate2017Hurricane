82039d7c237b7a1a14bccfe80e099aaabcaed13c
Tanner Driggers <tannerdriggers@ksu.edu>
2017-11-04 11:42:52 -0500

    Init

---
+++ Hurricane.py
@@ -0,0 +1,48 @@
+import json
+import re
+import geocoder
+import random
+from textblob import TextBlob
+
+table = []
+output = []
+
+with open('stream__hurricanemaria1(test).json', 'r') as data_file:
+    for line in data_file:
+        table.append(json.loads(line))
+i = 1
+cities = dict()
+for row in table:
+    tim = row['created_at']
+    index = tim.find('+')
+    tim = tim[:index-1:]
+
+    location = row['user']['location']
+    if location is None:
+        continue
+    else:
+        location = location.lower()
+
+    if str(location) == 'None' or '➡' in str(location):
+        lat_lng = (0.0, 0.0)
+    else:
+        if location in cities:
+            lat_lng = cities[location]
+        else:
+            lat_lng = geocoder.google(location).latlng
+            if not lat_lng:
+                lat_lng = (0.0, 0.0)
+            cities[location] = lat_lng
+
+    text = row['text']
+    if text[0] == 'R' and text[1] == 'T':
+        index = text.find(': ')
+        text = text[index+2:]
+    text = re.sub(r'http\S+', '', text)
+
+    sentiment = TextBlob(text).sentiment.polarity
+    output.append({'sentiment': sentiment, 'latitude': lat_lng[0] + random.randrange(-1, 1) * 0.1, 'longitude': lat_lng[1] + random.randrange(-1, 1) * 0.1})
+
+with open('OutputData.json', 'w') as f:
+    f.write(json.dumps(output))
+    print('Done')
